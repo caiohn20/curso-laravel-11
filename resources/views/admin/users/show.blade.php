d267dfbb35dc1caa0bb8d7e6904ac084467cc1f5
caiohn20 <caio.hn20@gmail.com>
2024-11-23 17:33:11 -0300

    feat: authorization usando o is-admin

--- resources/views/admin/users/show.blade.php
+++ resources/views/admin/users/show.blade.php
@@ -15,15 +15,13 @@
     </ul>
     <x-alert />
 
-    {{-- @can('owner', $user)
-        pode deletar
-    @endcan --}}
+    @can('is-admin', $user)
+        <form action="{{ route('users.destroy', $user->id) }}" method="post">
+            @csrf
+            @method('delete')
+            <button type="submit"
+                class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">Deletar</button>
+        </form>
+    @endcan
 
-
-    <form action="{{ route('users.destroy', $user->id) }}" method="post">
-        @csrf
-        @method('delete')
-        <button type="submit"
-            class="text-white bg-red-700 hover:bg-red-800 focus:outline-none focus:ring-4 focus:ring-red-300 font-medium rounded-full text-sm px-5 py-2.5 text-center me-2 mb-2">Deletar</button>
-    </form>
 @endsection
